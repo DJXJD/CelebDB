7d2a5663fbfdad6c404906bdde5147e7e127391a
DJXJD <djxjd5713@gmail.com>
2024-03-10 23:27:10 -0400

    Initialize Spring Boot project

---
+++ src/main/java/dev/djxjd/celebdb/CelebDbApplication.java
@@ -0,0 +1,13 @@
+package dev.djxjd.celebdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/djxjd/celebdb/CelebDbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.djxjd.celebdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

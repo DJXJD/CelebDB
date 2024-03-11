dfad6dfff55ac899c8296ac7e5c69d5b761070a8
DJXJD <djxjd5713@gmail.com>
2024-03-11 01:56:13 -0400

    Added a basic birthday scraping test page
    Data is scraped from Wikipedia

---
+++ src/main/java/dev/djxjd/celebdb/controllers/TestController.java
@@ -0,0 +1,40 @@
+package dev.djxjd.celebdb.controllers;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/")
+	public String getRoot() {
+		return "scrapeBDay";
+	}
+	
+	@PostMapping("/scrape")
+	public String scrapeBDay(@RequestParam String name, RedirectAttributes ra) {
+		Document wikiPage;
+		try {
+			wikiPage = Jsoup.connect("https://en.wikipedia.org/wiki/" + name).get();
+		} catch (IOException e) {
+			wikiPage = new Document("shit fucked up");
+			e.printStackTrace();
+		}
+		Element bday = wikiPage.selectFirst(".bday");
+		if (bday == null) ra.addFlashAttribute("error", "Person could not be found");
+		else ra.addFlashAttribute("bday", LocalDate.parse(bday.text()).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+		return "redirect:/";
+	}
+
+}

ce492a9919efa2b3fd10b61fc3aea9187216afec
DJXJD <djxjd5713@gmail.com>
2024-03-11 20:44:38 -0400

    Put scrape target in URL for persistence and easy shareability

--- src/main/java/dev/djxjd/celebdb/controllers/TestController.java
+++ src/main/java/dev/djxjd/celebdb/controllers/TestController.java
@@ -9,36 +9,32 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TestController {
 	
 	@GetMapping("/")
-	public String getRoot() {
+	public String getRoot(@RequestParam(required = false) String name, Model model) {
+		if (name != null) {
+			Document wikiPage;
+			try {
+				wikiPage = Jsoup.connect("https://en.wikipedia.org/wiki/" + name).get();
+			} catch (IOException e) {
+				wikiPage = new Document("shit fucked up");
+				e.printStackTrace();
+			}
+			Element bday = wikiPage.selectFirst(".bday");
+			if (bday == null) model.addAttribute("error", name + "'s birthday could not be found");
+			else {
+				model.addAttribute("bday", LocalDate.parse(bday.text()).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+				model.addAttribute("image", wikiPage.selectFirst(".infobox-image img").attr("src"));
+				model.addAttribute("name", wikiPage.selectFirst("h1 span").text());
+			}
+		}
 		return "scrapeBDay";
-	}
-	
-	@PostMapping("/scrape")
-	public String scrapeBDay(@RequestParam String name, RedirectAttributes ra) {
-		Document wikiPage;
-		try {
-			wikiPage = Jsoup.connect("https://en.wikipedia.org/wiki/" + name).get();
-		} catch (IOException e) {
-			wikiPage = new Document("shit fucked up");
-			e.printStackTrace();
-		}
-		Element bday = wikiPage.selectFirst(".bday");
-		if (bday == null) ra.addFlashAttribute("error", name + "'s birthday could not be found");
-		else {
-			ra.addFlashAttribute("bday", LocalDate.parse(bday.text()).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
-			ra.addFlashAttribute("image", wikiPage.selectFirst(".infobox-image img").attr("src"));
-			ra.addFlashAttribute("name", wikiPage.selectFirst("h1 span").text());
-		}
-		return "redirect:/";
 	}
 
 }
